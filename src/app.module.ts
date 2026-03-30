c97ded8e49a1e32d69de23a6f7ecf9356da37ac2
高島恭平 <kyohei_takashima@kyomac.local>
2026-03-30 15:19:53 +0900

    booksのデータ操作クラス雛形作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseConfig } from './database/database.config';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     // 環境変数の読み込み
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true, }),
     // TypeORM設定
-    TypeOrmModule.forRootAsync({
-      useClass: DatabaseConfig,
-    }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConfig, }),
+
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
