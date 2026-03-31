d02116cd4534049c3b1169c917b47a4c397c9e67
高島恭平 <kyohei_takashima@kyomac.local>
2026-03-31 20:12:55 +0900

    hashmapの実装方法追加

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,11 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Book } from "./entities/book.entity";
+import { User } from "../users/entities/user.entity";
 
 @Module({
   // Bookエンティティに紐付いたTypeORMの標準リポジトリ機能を有効化する
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book, User])],
   controllers: [BooksController],
   providers: [BooksService],
 })
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { Book } from './entities/book.entity';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from "./dto/update-book.dto";
+import { User } from "../users/entities/user.entity";
 
 @Injectable()
 export class BooksService {
   constructor(
       @InjectRepository(Book) // ここでRepositoryを注入
       private readonly bookRepository: Repository<Book>,
+      @InjectRepository(User)
+      private readonly userRepository: Repository<User>,
   ) {}
 
   // 1件作成
@@ -19,8 +22,21 @@ export class BooksService {
   }
 
   // 全件取得
-  findAll() {
-    return this.bookRepository.find();
+  async findAll() {
+    const books = await this.bookRepository.find();
+    const userIds = books.map(book => book.userId);
+    const users = await this.userRepository.findBy({ id: In(userIds) });
+    const usersHashMap = new Map<number, User>();
+    for (const user of users) {
+      usersHashMap.set(user.id, user);
+    }
+
+    return books.map(book => ({
+      id: book.id,
+      title: book.title,
+      author: usersHashMap.get(book.userId)?.name || 'Unknown',
+      isPublished: book.isPublished,
+    }))
   }
 
   // 1件取得
